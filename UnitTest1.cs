89775a4ba59f184f3fdf153164e30af0affcd517
Adam Ateye <S01760@newday.co.uk>
2022-09-13 16:37:55 +0100

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,43 @@
+namespace EmployeeReportKata
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void GetAdultEmployees()
+        {
+            //Arrange
+            Employee employee = new Employee();
+            //Act
+            var possibleSundayWorkers = employee.CheckAge();
+            //Assert
+            Assert.Equal(2, possibleSundayWorkers);
+        }
+    }
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Employee(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public Employee() { }
+
+        public List<Employee> employees = new List<Employee>()
+    {
+        new Employee ("max", 17),
+        new Employee ("sepp", 18),
+        new Employee ("nina", 15),
+        new Employee ("mike", 51)
+
+    };
+
+        public int CheckAge() => employees.Where(Employee => Employee.Age >= 18).Count();
+
+    }
+
+
+}
